c7eea3422c6b14ef7a560aa0f4cea29dea8b2abd
gabrielestrela <gabrielestrela92@gmail.com>
2017-11-21 20:30:54 -0200

    Started gyroscope implementation

--- app/src/main/java/br/com/whatsappandroid/cursoandroid/focar/AviaoActivity.java
+++ app/src/main/java/br/com/whatsappandroid/cursoandroid/focar/AviaoActivity.java
@@ -4,6 +4,10 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,8 +15,14 @@ import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class AviaoActivity extends AppCompatActivity {
+
+    private SensorManager sm;
+    private Sensor gyro;
+    private SensorEventListener gyroEListener;
+    private Switch s;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +31,7 @@ public class AviaoActivity extends AppCompatActivity {
 
         createCircle();
 
-        Switch s = (Switch) findViewById(R.id.onOff);
+        s = (Switch) findViewById(R.id.onOff);
 
         Boolean sState = s.isChecked();
 
@@ -33,11 +43,50 @@ public class AviaoActivity extends AppCompatActivity {
                     sText.setText("Desativado");
                 }else{
                     sText.setText("Ativado");
+                    initializeGyro();
                 }
             }
         });
 
     }
+
+    public void initializeGyro() {
+        sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+        gyro = sm.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        if(gyro == null) {
+            Toast.makeText(this, "The device has no Gyroscope !", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        gyroEListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                if(sensorEvent.values[2] > 0.5f) {
+                    getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+                }else if(sensorEvent.values[2] < - 0.5f) {
+                    getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+    }
+
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        sm.registerListener(gyroEListener, gyro, SensorManager.SENSOR_DELAY_FASTEST);
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        sm.unregisterListener(gyroEListener);
+//    }
 
     public void createCircle(){
         ImageView circle = (ImageView) findViewById(R.id.rCircle);
