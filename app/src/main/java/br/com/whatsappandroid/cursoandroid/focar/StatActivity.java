640ea71b7ca2f75e379e5f9aa0eca78271f6dd63
gabrielestrela <gabrielestrela92@gmail.com>
2017-12-02 19:07:52 -0200

    Fixed graph error

--- app/src/main/java/br/com/whatsappandroid/cursoandroid/focar/StatActivity.java
+++ app/src/main/java/br/com/whatsappandroid/cursoandroid/focar/StatActivity.java
@@ -3,14 +3,35 @@ package br.com.whatsappandroid.cursoandroid.focar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StatActivity extends AppCompatActivity {
+
+    private LineChart chart;
+    private int[] data; // for further creation and integration with database
+    List<Entry> entries;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stat);
+
+        chart = (LineChart) findViewById(R.id.chart);
+        entries = new ArrayList<Entry>();
+
+        // TODO
+//        for(int d: data) {
+            // turn your data into Entry objects
+//            entries.add(new Entry(d.getValueX(), d.getValueY()));
+//        }
+
+
+
     }
 }
